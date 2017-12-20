112a8c914b04d3e58ef6bbb3171a7b5cc6c13bf7
Ândriu Coelho <andriu_coelho1993@hotmail.com>
2017-12-20 11:08:49 -0200

    first commit

---
+++ Agenda/Alunos/Controller/AlunoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AlunoViewController.swift
+//  Agenda
+//
+//  Created by Ândriu Coelho on 24/11/17.
+//  Copyright © 2017 Alura. All rights reserved.
+//
+
+import UIKit
+
+class AlunoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
